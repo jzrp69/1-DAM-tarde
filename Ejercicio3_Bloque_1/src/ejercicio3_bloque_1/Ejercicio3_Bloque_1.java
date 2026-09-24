eeea14ca4e4ef6deb0d9f95de4ca487874c36329
Alumno Tarde <Alumno Tarde@LB-DAW2-10.iesluisbraille.es>
2026-09-24 16:44:14 +0200

    Ejercicio Listo

--- Ejercicio3_Bloque_1/src/ejercicio3_bloque_1/Ejercicio3_Bloque_1.java
+++ Ejercicio3_Bloque_1/src/ejercicio3_bloque_1/Ejercicio3_Bloque_1.java
@@ -4,6 +4,8 @@
  */
 package ejercicio3_bloque_1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Alumno Tarde
@@ -15,7 +17,30 @@ public class Ejercicio3_Bloque_1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int numero;
+        int hormigas; 
+        int arañas;
+        int cochinillas;
+        int total;
+        Scanner sc = new Scanner(System.in);
         
+        System.out.println("Cuantas hormigas ha capturado hoy? :");
+            hormigas = sc.nextInt() *6;
+        
+        System.out.println("Cuantas arañas ha capturado hoy? :");
+            arañas = sc.nextInt() *8;
+        
+        System.out.println("Cuantas cochinillas ha capturado hoy? :");
+            cochinillas = sc.nextInt()*14;
+      
+        
+        total = hormigas+arañas+cochinillas;
+        
+        System.out.println("En total hoy has capturado: " + total);
+                
+        
+                
+                
     }
     
 }
